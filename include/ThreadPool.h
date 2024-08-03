de057aa56c85cd0fc3aee2fd51b3981acda0d36b
shiliulong <2860863870@qq.com>
2024-08-03 14:43:09 +0800

    优化

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -31,3 +31,26 @@ private:
     bool stop;
 };
 
+// 添加新任务到任务队列
+template<class F, class... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type> // 推导函数返回类型
+{
+    using return_type = typename std::result_of<F(Args...)>::type;
+
+    auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+
+    std::future<return_type> res = task->get_future();
+
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex);
+
+        if(stop)
+        {
+            throw std::runtime_error("enqueue on stopped ThreadPool");
+        }
+
+        tasks.emplace([task](){(*task)();});
+    }
+    condition.notify_one(); //触发之后，直到有等待线程
+    return res;
+}
